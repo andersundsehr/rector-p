dabf50fc9ad4a46cca4104f9441c9aa8bbd61d71
Automated Splitter <m.vogel@andersundsehr.com>
2025-12-02 10:33:57 +0000

    Merge pull request #100 from andersundsehr/fix-pipeline | Matthias Vogel <m.vogel@andersundsehr.com> | 2025-12-02 11:32:15 +0100 | https://github.com/andersundsehr/grumphp-config/commit/ffcd6e7107c789ae657f5a3dd6e13a4f697a74e6

--- src/Dto/Task.php
+++ src/Dto/Task.php
@@ -7,7 +7,10 @@ namespace Andersundsehr\RectorP\Dto;
 enum Task: string
 {
     case execute = 'execute';
+
     case skipFile = 'skip-file';
+
     case retry = 'retry';
+
     case quit = 'quit';
 }
